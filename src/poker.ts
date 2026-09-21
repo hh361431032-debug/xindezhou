73285b3339067d67fcab2d33d5c2fb438fbea3aa
hh361431032-debug <hh361431032@gmail.com>
2026-09-21 20:41:34 +0800

    Import Texas Hold'em web: src/poker.ts

---
+++ src/poker.ts
@@ -0,0 +1,21 @@
+import { CardType, Suit, Rank } from './types';
+
+const SUITS: Suit[] = ['spades', 'hearts', 'diamonds', 'clubs'];
+const RANKS: Rank[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+export const createDeck = (): CardType[] => {
+  const deck: CardType[] = [];
+  for (const suit of SUITS) {
+    for (const rank of RANKS) deck.push({ suit, rank });
+  }
+  return shuffle(deck);
+};
+
+export const shuffle = (deck: CardType[]): CardType[] => {
+  const newDeck = [...deck];
+  for (let i = newDeck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newDeck[i], newDeck[j]] = [newDeck[j], newDeck[i]];
+  }
+  return newDeck;
+};
